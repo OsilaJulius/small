65e719e0bc2a898ef386d2cf680aa9e6c01e608c
OsilaJulius <osilajules@gmail.com>
2017-06-06 17:02:06 +0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   resources :users
 
-  get 'users/new'
-
-  get 'users/show'
-
-  get 'users/edit'
+  # User routes
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/edit', to: 'users#edit', as: 'user_edit'
+  patch '/users/:id/edit', to: 'users#update', as: 'user_update'
 
   # Static pages routes
   root 'static_pages#home'
-  get 'about', to: 'static_pages#about'
-  get 'contact', to: 'static_pages#contact'
-  get 'help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
 end
