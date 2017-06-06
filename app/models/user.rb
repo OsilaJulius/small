bc3c81ab4346ccc3cd21cf6bb2f0f1a3f3314453
OsilaJulius <osilajules@gmail.com>
2017-06-06 19:13:14 +0800

    Create and test basic user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 },
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
 end
