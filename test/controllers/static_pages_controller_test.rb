9788b5d731303f1eed82096d073be47704a317e7
OsilaJulius <osilajules@gmail.com>
2017-06-06 17:54:50 +0800

    Edit application helper, static pages controller test, and users controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select 'title', 'Small'
+    assert_select 'title', 'Small!'
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select 'title', 'About | Small'
+    assert_select 'title', 'About | Small!'
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select 'title', 'Help | Small'
+    assert_select 'title', 'Help | Small!'
   end
 
   test "should get contact" do
     get contact_url
     assert_response :success
-    assert_select 'title', 'Contact | Small'
+    assert_select 'title', 'Contact | Small!'
   end
 end
