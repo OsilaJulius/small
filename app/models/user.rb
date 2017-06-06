2fcbe0df06b42cfdbcef7afe4b4be46b3959eb84
OsilaJulius <osilajules@gmail.com>
2017-06-06 19:41:43 +0800

    Add and test password digest of users database

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 50 },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   private
     def downcase_email
